c514fdc5a4f9c0e97b23843295ea0d48815e17f5
Vkarnan <vkarnan031@gmail.com>
2026-07-07 11:03:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,13 @@ const CACHE_NAME = 'viking-cache-v12';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './background.webp'
 ];
 
+// Handles standard offline installation caching
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,6 +17,7 @@ self.addEventListener('install', (e) => {
   );
 });
 
+// Responds with cached assets instantly when offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
